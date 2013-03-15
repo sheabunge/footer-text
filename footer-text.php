84d7f7e2c6d8c77e42f25ec5bbc4500e5884f2f1
Shea Bunge <info@bungeshea.com>
2013-03-15 15:24:25 +1100

    Improved template tags and function docs
    Properly format footer text output
    Allow content before and after footer text

--- footer-text.php
+++ footer-text.php
@@ -10,6 +10,16 @@
  * Version: 1.0
  */
 
+/** Dashboard Administration Menu ********************************************/
+
+/**
+ * Add the footer text options page to
+ * the 'Appearance' dashboard menu
+ *
+ * @uses add_theme_page() To register the new submenu
+ *
+ * @since 1.0
+ */
 function add_footer_text_options_page() {
 	$theme_page = add_theme_page(
 		__( 'Footer Text', 'footer-text' ),	// Name of page
@@ -21,7 +31,18 @@ function add_footer_text_options_page() {
 }
 add_action( 'admin_menu', 'add_footer_text_options_page' );
 
-
+/**
+ * Display the footer text options page
+ * and save posted text to the database
+ *
+ * @uses update_option() To save the text to the database
+ * @uses screen_icon() To display the dashboard menu icon
+ * @uses wp_editor() For a visual editor
+ * @uses get_option() To retrieve the current text from the database
+ * @uses submit_button() To generate a form submit button
+ *
+ * @since 1.0
+ */
 function render_footer_text_options_page() {
 
 	if ( isset( $_POST['footer_text'] ) )
@@ -43,11 +64,41 @@ function render_footer_text_options_page() {
 	<?php
 }
 
+/** Template Tags ******************************************************/
 
+/**
+ * Fetches the footer text from the database
+ * with formatting functions applied
+ *
+ * @return string The formatted footer text
+ *
+ * @since 1.0
+ */
 function get_footer_text() {
-	return get_option( 'theme_footer_text', '' );
+	return apply_filters( 'the_content', get_option( 'theme_footer_text', '' ) );
 }
 
-function footer_text() {
-	echo get_footer_text();
+/**
+ * Retrieves the footer text and displays it if it is set
+ * Nothing is displayed if the footer text is not set
+ *
+ * @uses get_footer_text() To retrieve the footer text
+ *
+ * @param string $before The text to display before the footer text
+ * @param string $after The text to display after the footer text
+ * @param bool $display Output or return the resulting text?
+ * @return null|string The footer text if $output is true
+ *
+ * @since 1.0
+ */
+function footer_text( $before = '', $after = '', $display = true ) {
+
+    $footer_text = get_footer_text();
+    $output = ( empty( $footer_text ) ? '' : $before . $footer_text . $after );
+
+    if ( $display )
+	    echo $output;
+    else
+        return $output;
+
 }
