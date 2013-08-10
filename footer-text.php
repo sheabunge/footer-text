44106d4063eb473d7676cf5ef6c45ecf83de52fc
Shea Bunge <info@bungeshea.com>
2013-08-10 10:17:25 +1000

    Fixed comment alignment

--- footer-text.php
+++ footer-text.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name: Footer Text
- * Plugin URI: http://bungeshea.com/plugins/footer-text/
+ * Plugin URI:  http://bungeshea.com/plugins/footer-text/
  * Description: Allow changing of the theme footer text easily from the dashboard
- * Author: Shea Bunge
- * Author URI: http://bungeshea.com
- * License: MIT
- * License URI: http://opensource.org/licenses/mit-license.php
- * Version: 1.0
+ * Author:      Shea Bunge
+ * Author URI:  http://bungeshea.com
+ * License:     MIT
+ * License URI: http://opensource.org/licenses/MIT
+ * Version:     1.0
  */
 
 /**
@@ -47,6 +47,7 @@ function add_footer_text_caps() {
 	foreach ( $roles as $role ) {
 		/* Retrieve the editor role to add the cap to */
     	$role = get_role( $role );
+
     	/* Add the capability to edit footer text */
     	$role->add_cap( 'edit_footer_text' );
     }
@@ -57,14 +58,12 @@ add_action( 'admin_init', 'add_footer_text_caps');
  * Add the footer text options page to
  * the 'Appearance' dashboard menu
  *
- * @uses add_theme_page() To register the new sub-menu
- *
+ * @uses   add_theme_page() To register the new sub-menu
  * @return void
- *
- * @since 1.0
+ * @since  1.0
  */
 function add_footer_text_options_page() {
-	$theme_page = add_theme_page(
+	add_theme_page(
 		__( 'Footer Text', 'footer-text' ),
 		__( 'Footer Text', 'footer-text' ),
 		'edit_footer_text',
@@ -78,15 +77,15 @@ add_action( 'admin_menu', 'add_footer_text_options_page' );
  * Display the footer text options page
  * and save posted text to the database
  *
- * @uses update_option() To save the text to the database
- * @uses screen_icon() To display the dashboard menu icon
- * @uses wp_editor() For a visual editor
- * @uses get_option() To retrieve the current text from the database
- * @uses submit_button() To generate a form submit button
+ * @uses   update_option() To save the text to the database
+ * @uses   screen_icon()   To display the dashboard menu icon
+ * @uses   wp_editor()     For a visual editor
+ * @uses   get_option()    To retrieve the current text from the database
+ * @uses   submit_button() To generate a form submit button
  *
  * @return void
  *
- * @since 1.0
+ * @since  1.0
  */
 function render_footer_text_options_page() {
 
@@ -114,17 +113,17 @@ function render_footer_text_options_page() {
  * Fetches the footer text from the database
  * with formatting functions applied
  *
- * @param string $default What to use if no footer text is set
- * @return string The formatted footer text
+ * @param  string $default What to use if no footer text is set
+ * @return string          The formatted footer text
  *
- * @since 1.0
+ * @since  1.0
  */
 function get_footer_text( $default = '' ) {
 
-	// retrieve the footer text from the database
+	/* Retrieve the footer text from the database */
 	$footer_text = get_option( 'theme_footer_text', $default );
 
-	// filter and return the text
+	/* Filter and return the text */
 	return apply_filters( 'get_footer_text', $footer_text );
 }
 
@@ -132,14 +131,14 @@ function get_footer_text( $default = '' ) {
  * Retrieves the footer text and displays it if it is set
  * Nothing is displayed if the footer text is not set
  *
- * @uses get_footer_text() To retrieve the footer text
+ * @uses   get_footer_text() To retrieve the footer text
  *
- * @param string $before The text to display before the footer text
- * @param string $after The text to display after the footer text
- * @param string $default What to display if no text is set
+ * @param  string $before    The text to display before the footer text
+ * @param  string $after     The text to display after the footer text
+ * @param  string $default   What to display if no text is set
  * @return void
  *
- * @since 1.0
+ * @since  1.0
  */
 function footer_text( $before = '', $after = '', $default = '' ) {
 
@@ -155,11 +154,9 @@ function footer_text( $before = '', $after = '', $default = '' ) {
  * Returns a formatted link to
  * the current page's permalink
  *
- * @uses get_permalink()
- *
+ * @uses   get_permalink()
  * @return string
- *
- * @since 1.0
+ * @since  1.0
  */
 function footer_text_shortcode_permalink() {
 	$label = ( isset( $atts ) ? (string) $atts : get_permalink() );
@@ -170,11 +167,9 @@ function footer_text_shortcode_permalink() {
  * Returns the date when the current page
  * was last modified
  *
- * @uses the_modified_date()
- *
+ * @uses   the_modified_date()
  * @return string
- *
- * @since 1.0
+ * @since  1.0
  */
 function footer_text_shortcode_last_modified() {
 	return the_modified_date( 'd/m/Y', '<time>', '</time>', false );
@@ -185,8 +180,7 @@ function footer_text_shortcode_last_modified() {
  * a copyright notice
  *
  * @return string
- *
- * @since 1.0
+ * @since  1.0
  */
 function footer_text_shortcode_current_year() {
 	return '<time>' . date( 'Y' ) . '</time>';
@@ -197,22 +191,22 @@ function footer_text_shortcode_current_year() {
  * on the footer text, paired with their callbacks
  *
  * @return array
- *
  * @since 1.0
  */
 function get_footer_text_shortcode_tags() {
-	return apply_filters( 'footer_text_shortcode_tags', array(
+	$shortcode_tags = array(
 		'last_modified' => 'footer_text_shortcode_last_modified',
-		'page_link' => 'footer_text_shortcode_permalink',
-		'year' => 'footer_text_shortcode_current_year',
-	) );
+		'page_link'     => 'footer_text_shortcode_permalink',
+		'year'          => 'footer_text_shortcode_current_year',
+	);
+
+	return apply_filters( 'footer_text_shortcode_tags', $shortcode_tags );
 }
 
 /**
  * Registers our shortcodes with WordPress
  *
- * @uses add_shortcode()
- *
+ * @uses  add_shortcode()
  * @since 1.0
  */
 function add_footer_text_shortcodes() {
@@ -227,12 +221,12 @@ add_action( 'init', 'add_footer_text_shortcodes' );
  * Removes our custom shortcodes from the post content
  * so they don't interfere with anything
  *
- * @link http://justintadlock.com/archives/2013/01/08/disallow-specific-shortcodes-in-post-content
+ * @link   http://justintadlock.com/archives/2013/01/08/disallow-specific-shortcodes-in-post-content
  *
  * @param  string $content The post content with the custom shortcodes
- * @return string The post content without the custom shortcodes
+ * @return string          The post content without the custom shortcodes
  *
- * @since 1.0
+ * @since  1.0
  */
 function footer_text_post_content_remove_shortcodes( $content ) {
 
@@ -251,12 +245,12 @@ add_filter( 'the_content', 'footer_text_post_content_remove_shortcodes', 0 );
 /**
  * Adds our shortcodes back to the post content when it's safe
  *
- * @see http://justintadlock.com/archives/2013/01/08/disallow-specific-shortcodes-in-post-content
+ * @see    http://justintadlock.com/archives/2013/01/08/disallow-specific-shortcodes-in-post-content
  *
  * @param  string $content The post content without the custom shortcodes
- * @return string The post content with the custom shortcodes
+ * @return string          The post content with the custom shortcodes
  *
- * @since 1.0
+ * @since  1.0
  */
 function footer_text_post_content_add_shortcodes( $content ) {
 
